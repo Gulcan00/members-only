8f3f6f060405930956f0e02b49efebd82bc20b03
Gulcan00 <zipwire1@hotmail.com>
2025-03-14 20:15:28 +0200

    Complete initial set up of express app.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
+import 'dotenv/config';
 import express from 'express';
+import ejs from 'ejs';
+import path from 'path';
+
+const __dirname = path.resolve();
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+// Error Handling
